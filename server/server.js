47b72cdedae2d4770b8c2d95b5000a82957ff047
Parth Chaturvedi <parthchaturvedi0204@gmail.com>
2026-04-19 19:43:25 +0530

    server setup changes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import connectDB from "./src/config/database.js";
 dotenv.config();
 connectDB();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
